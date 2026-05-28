822b6a4e1b12e0f47344bfdddfca782aaeca0e3d
Bhawesh Kumar <createbhawesh@gmail.com>
2026-05-28 08:37:51 +0530

    add

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+terraform {
+required_providers {
+   azurerm = {
+   source = "hashicorp/azurerm"
+    version = "4.71.0"
+  }
+ }
+}
+provider azurerm {
+ features {}
+}
 resource "azurerm_resource_group" "pappi"{
 for_each = toset(["rg1","rg2","rg3"])
 name = each.value
 location = "easteurope"
 }
+
 
